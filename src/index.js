e273b6c55e7abe193ac8f05e59a50e8e9d5e37a4
MrChae1 <ljnellam27@gmail.com>
2023-10-21 02:23:12 +0700

    remove edit class stylein the section tagsrc/style/style.scss

--- src/index.js
+++ src/index.js
@@ -31,13 +31,10 @@ function component(){
     asideTag.innerHTML = `
         <main> 
             <section> 
-            hi
             </section>
-            <section>
-            he 
+            <section class="tasksSection">
             </section>
-            <section>
-            ha  
+            <section class="notesSection"> 
             </section>
             <svg class="asideSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus-circle</title><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>   
         </main>
