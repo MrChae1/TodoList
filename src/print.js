87fb2c352c34169b5c89e722622e43a13d05e125
MrChae1 <ljnellam27@gmail.com>
2023-10-21 02:22:20 +0700

    section tag to manipulate the dom

--- src/print.js
+++ src/print.js
@@ -10,7 +10,7 @@ export function forModals(mainCon){
                 <h4>X</h4>
             </header>
             <nav class="nav-modal">
-                <button class="C-tasks special-Nav">TASKS</button>
+                <button class="C-tasks special-btn">TASKS</button>
                 <button class="C-notes">NOTES</button>
             </nav>
             <div class="sub-modals">
@@ -26,7 +26,7 @@ export function forModals(mainCon){
                     <div class="tasks-Prioprity">
                         <h3>Priority:</h3>
                         <div class="prio-btn">
-                            <button class="low selected-prio" value="low">LOW</button>
+                            <button class="low special-btn" value="low">LOW</button>
                             <button class="medium" value="medium">MEDIUM</button>
                             <button class="high" value="high">HIGH</button>
                         </div>     
@@ -42,26 +42,26 @@ export function forModals(mainCon){
                     <button class="modals-add">ADD NEW</button>
                 </div>
             </div>
-            <div class="errorPopUp">
-                <header>
-                    <h2>Something Missing</h2>
-                    <h4 class="inerror">X</h4>
-                </header>
-                <div class="errorMessage">
-                    <p>Please provide needed data....</p>
-                </div> 
-            </div>
         </div>
         `;
-        // Import the 'command' module once at the beginning of your script
-        import('./command')
-        .then((module) => {
+        const aside = mainCon.querySelector('aside');
+        const allSection = Array.from(aside.querySelectorAll('section'));
         const modalHeader = modalTag.querySelector('header');
         const modalNav = modalTag.querySelector('nav');
         const MNavbtn = Array.from(modalNav.querySelectorAll('button'));
         const mainSubmodals = Array.from(modalTag.querySelectorAll('.main-sub'));
         const exitBtn = modalHeader.querySelector('h4');
-
+        const addBtn = modalTag.querySelector('.modals-add');
+        const tasksModals = modalTag.querySelector('.tasks-modals');
+        const allTasksData = Array.from(tasksModals.querySelectorAll('*'));
+        const tasksInputs = [allTasksData[1], allTasksData[2], allTasksData[5]];
+        const notesModals = modalTag.querySelector('.notes-modals');
+        const allNotesData = Array.from(notesModals.querySelectorAll('*'));
+        const notesInputs = [allNotesData[1], allNotesData[2]];
+        const prioBtn = Array.from(allTasksData[8].querySelectorAll('button'));
+        // Import the 'command' module once at the beginning of your script
+        import('./command')
+        .then((module) => {
         modalNav.addEventListener('click', (event) => {
             module.removeClass(MNavbtn);
             module.changeNav(event, MNavbtn);
@@ -71,6 +71,23 @@ export function forModals(mainCon){
         exitBtn.addEventListener('click', () => {
             module.exitBtn(modalTag);
         });
+
+        allTasksData[8].addEventListener('click', function(event){
+            module.removeClass(prioBtn);
+            module.changeNav(event, prioBtn);
+        });
+
+        addBtn.addEventListener('click', () =>{
+            if(MNavbtn[0].classList.contains('special-btn')){
+                const selectedPrio = allTasksData[8].querySelector('.special-btn');
+                module.verifyValue(tasksInputs, MNavbtn, selectedPrio, modalTag, allSection[1]);
+
+            }
+            else{
+                module.verifyValue(notesInputs, MNavbtn, null, modalTag, allSection[2]);
+            }
+        });
+
         })
         .catch((error) => {
         console.error('Error while importing module: ', error);
