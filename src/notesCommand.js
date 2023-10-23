bae8a22b2df593af87c8ca930c840592c9a37b0e
MrChae1 <ljnellam27@gmail.com>
2023-10-24 00:12:56 +0700

    Add new function that the user can edit the notes

--- src/notesCommand.js
+++ src/notesCommand.js
@@ -4,4 +4,36 @@ export const NotesDelete = (main, NotesArray, notesIndex) => {
     });
     NotesArray.splice(notesIndex, 1);
     console.log(NotesArray);
-} 
+}
+
+export const EditNotes = (NotesArray, NotesObj, NotesD) => {
+    const allNotesContent = Array.from(NotesD.querySelectorAll('*'));
+    const editDiv = document.createElement('div');
+    editDiv.classList.add('editDiv');
+    editDiv.innerHTML = `
+        <div class="EditContainer">
+            <h4>X</h4>
+            <input type='text' value='${NotesObj.title}'class='NotesHeader'>
+            <textarea  cols="30" rows="10" value='${NotesObj.desc}' class='notesEditdesc'></textarea>
+        </div>
+    `;
+    const inputEdit = editDiv.querySelector('input'); 
+    const textA = editDiv.querySelector('textarea');
+    const editExit = editDiv.querySelector('h4');
+    textA.value = NotesObj.desc;
+    editExit.addEventListener('click', () => {
+        NotesArray[NotesObj.index].title = inputEdit.value;
+        NotesArray[NotesObj.index].desc = textA.value;
+        allNotesContent[1].textContent = inputEdit.value;
+        allNotesContent[4].textContent = textA.value;
+
+        import('./command').then(module => {
+            module.exitBtn(editDiv);
+        });
+    });
+    document.body.appendChild(editDiv);
+}
+
+const changeEdit = () => {
+
+}
