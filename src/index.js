a6dd0d27b9d2fa60b6e367c46964f99ed2977f81
MrChae1 <ljnellam27@gmail.com>
2023-10-25 21:33:44 +0700

    append the function in the home section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style/style.scss';
-import {updateDynamicDate, tasksToday} from './HomeSection'; 
+import {updateDynamicDate} from './HomeSection';
+import { tasksToday } from './command'; 
 
 function component(){ 
     const mainContainer = document.createElement('main');
@@ -53,7 +54,8 @@ function component(){
             <svg class="asideSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus-circle</title><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>   
         </main>
     `;
-
+    const articleOne = asideTag.querySelector('.artOne');
+    articleOne.append(tasksToday())
     const allSection = Array.from(asideTag.querySelectorAll('section'));
     const plusIcon = asideTag.querySelector('.asideSVG');
     plusIcon.addEventListener('click', () =>{
