59b1d40784864328b393b9ac304d9200c2b35446
MrChae1 <ljnellam27@gmail.com>
2023-10-30 23:10:02 +0700

    Remove the tag iniside the Home Section

--- src/index.js
+++ src/index.js
@@ -33,19 +33,6 @@ function component(){
     asideTag.innerHTML = `
         <main> 
             <section> 
-                <div>
-                    <header>
-                        ${updateDynamicDate()}
-                    </header>
-                    <div class="main-art">
-                        <div class="artOne">
-                            <h5>Tasks Today</h5>
-                        </div>
-                        <div class="artTwo">
-                            <h5>Pin Notes</h5>
-                        </div>
-                    </div>
-                </div> 
             </section>
             <section class="tasksSection">
             </section>
@@ -54,8 +41,6 @@ function component(){
             <svg class="asideSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus-circle</title><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>   
         </main>
     `;
-    const articleOne = asideTag.querySelector('.artOne');
-    articleOne.append(tasksToday())
     const allSection = Array.from(asideTag.querySelectorAll('section'));
     const plusIcon = asideTag.querySelector('.asideSVG');
     plusIcon.addEventListener('click', () =>{
