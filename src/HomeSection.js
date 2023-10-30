8f3ffff2cca53760b276ddc50e9a622fb9fe866d
MrChae1 <ljnellam27@gmail.com>
2023-10-30 23:08:48 +0700

    Remove function

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -1,22 +1,22 @@
-function getDynamicDate() {
-    const currentDate = new Date();
+// function getDynamicDate() {
+//     const currentDate = new Date();
   
-    // Get the current date in the format "YYYY-MM-DD"
-    const year = currentDate.getFullYear();
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = currentDate.getDate().toString().padStart(2, '0');
-    const formattedDate = `${year}-${month}-${day}`;
+//     // Get the current date in the format "YYYY-MM-DD"
+//     const year = currentDate.getFullYear();
+//     const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+//     const day = currentDate.getDate().toString().padStart(2, '0');
+//     const formattedDate = `${year}-${month}-${day}`;
   
-    return formattedDate;
-  }
+//     return formattedDate;
+//   }
   
-  export function updateDynamicDate() {
-    const dynamicDate = getDynamicDate();
-    return dynamicDate;
-  }
+//   export function updateDynamicDate() {
+//     const dynamicDate = getDynamicDate();
+//     return dynamicDate;
+//   }
   
-  // Update the dynamic date every 24 hours (in milliseconds)
-  setInterval(updateDynamicDate, 24 * 60 * 60 * 1000);
+//   // Update the dynamic date every 24 hours (in milliseconds)
+//   setInterval(updateDynamicDate, 24 * 60 * 60 * 1000);
 
   
 
