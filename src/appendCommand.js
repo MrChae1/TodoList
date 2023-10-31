458fbc47a25fb7406b0d800621b13f42a188b2d9
MrChae1 <ljnellam27@gmail.com>
2023-11-01 02:39:44 +0700

    Adding functionality if the notes and tasks was null in local storage

--- src/appendCommand.js
+++ src/appendCommand.js
@@ -1,16 +1,45 @@
 import './style/style.scss';
 
-let tasksArray = [];
-let notesArray = [];
-
 export const tasksLocalStorage = (tasksSection) => {
 
+    if(localNull() !== 0){
+        for(const localTasks of localNull()){
+            import('./command').then(module => {
+                module.appendTasks(localTasks, tasksSection);
+            });
+        }
+    }
 }
 
 export const notesLocalStorage = (notesSection) => {
-
+    if(notNull() !== 0){
+        for(const localNotes of notNull()){
+            import('./command').then(module => {
+                module.appendTasks(localNotes, notesSection);
+            });
+        }
+    }
 }
 
+export const localNull = () => {
+    const localItems = JSON.parse(localStorage.getItem('tasks'));
+    if(localItems != null){
+        return localItems;
+    }
+    else{
+        return 0;
+    }
+}
+
+export const notNull = () => {
+    const localItems = JSON.parse(localStorage.getItem('notes'));
+    if(localItems != null){
+        return localItems;
+    }
+    else{
+        return 0;
+    }
+}
 
 export const updateLocal = (thisArray, wArr) => {
     const appendThis = JSON.stringify(thisArray);
